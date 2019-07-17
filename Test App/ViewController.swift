a601169d003ce60eef3f664a7a93d0e17da0017a
Kevin M <kmarx@caltech.edu>
2019-07-17 13:58:17 -0700

    messed around with the graph

--- Test App/ViewController.swift
+++ Test App/ViewController.swift
@@ -7,14 +7,71 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var barChart: BarChartView!
+    @IBOutlet weak var MaxAmt: UISlider!
+    @IBOutlet weak var RefreshButton: UIButton!
+    
+    var valueArr = [Double]()
+    
+    @IBAction func GraphAnimate(_ sender: UIButton) {
+        let plot = getYVals()
+        for i in 0...100 {
+            
+        }
+    }
+    @IBAction func UpdateChart(_ sender: UISlider) {
+        let val = Int(sender.value)
+        barMaker(chartBar: barChart, value: val)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        barMaker(chartBar: barChart, value: 0)
+        
     }
-
 
 }
 
+func displayChart(chartBar: BarChartView, dataList: Array<Double>) {
+    var plotData = [ChartDataEntry]()
+    for (index, value) in dataList.enumerated() {
+        let chartEntry = BarChartDataEntry(x: Double(index), y: value)
+        plotData.insert(chartEntry, at: 0)
+    }
+    let dataSet = BarChartDataSet(entries: plotData, label: "haha yes")
+    let data = BarChartData(dataSets: [dataSet])
+    chartBar.data = data
+    chartBar.chartDescription?.text = "random stuff"
+    chartBar.leftAxis.axisMaximum = 1
+    chartBar.leftAxis.axisMinimum = -1
+    
+    chartBar.notifyDataSetChanged()
+}
+
+func getYVals() -> Array<Double> {
+    var values: Array<Double>
+    for i in 0...99 {
+        values.insert(Double(sin(Double(i)/Double(values) * 2 * Double.pi)), at: 0)
+    }
+    
+    return values
+}
+
+func barMaker(chartBar: BarChartView, value: Int) {
+    var plotData = [ChartDataEntry]()
+    for i in 0...99 {
+        plotData.insert(BarChartDataEntry(x: Double(i), y: Double(sin(Double(i)/Double(value) * 2 * Double.pi))), at: 0)
+    }
+    
+    let dataSet = BarChartDataSet(entries: plotData, label: "haha yes")
+    let data = BarChartData(dataSets: [dataSet])
+    chartBar.data = data
+    chartBar.chartDescription?.text = "random stuff"
+    chartBar.leftAxis.axisMaximum = 1
+    chartBar.leftAxis.axisMinimum = -1
+    
+    chartBar.notifyDataSetChanged()
+}
